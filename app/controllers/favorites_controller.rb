6c44d7830e2b8914a4cd38d171da56055679c602
devakdo <you@example.com>
2015-08-14 16:38:47 +0530

    changes applied for delete button in dashboard

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,16 +3,13 @@ class FavoritesController < ApplicationController
     fp = FavoriteProject.new
     fp.user = current_user
     fp.project = Project.where(id: params[:project_id]).first
-
     respond_to do |format|
       format.json { render json: fp.save.to_json }
     end
   end
-
   def destroy
-  	@favorite_project = FavoriteProject.where(project_id: params[:id],user_id: current_user.id).first
-  	@favorite_project.destroy
-  	redirect_to dashboard_path
+    @favorite_project = FavoriteProject.where(project_id: params[:id], user_id: current_user.id).first
+    @favorite_project.destroy
+    redirect_to dashboard_path
   end
-
 end
