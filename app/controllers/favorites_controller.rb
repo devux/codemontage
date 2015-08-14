744def55268b4cb1da7a621697c04a3e83a112a1
devakdo <you@example.com>
2015-08-14 16:57:18 +0530

    modification done

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,5 @@
 class FavoritesController < ApplicationController
+  
   def create
     fp = FavoriteProject.new
     fp.user = current_user
@@ -7,8 +8,11 @@ class FavoritesController < ApplicationController
       format.json { render json: fp.save.to_json }
     end
   end
+  
   def destroy
-    @favorite_project = FavoriteProject.where(project_id: params[:id], user_id: current_user.id).first
+    @favorite_project = FavoriteProject.where(project_id: params[:id],
+                                              user_id: current_user.id)
+                                       .first
     @favorite_project.destroy
     redirect_to dashboard_path
   end
